e17918dcac9cb7ed408b737bd7272002b19c9369
Cyril Vlaminck <developer@cvlaminck.fr>
2014-02-15 11:39:53 +0100

    Useless commit

--- core/firmware.X/nbproject/Makefile-default.mk
+++ core/firmware.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=main.c ndEventBusCore/ndEventBusCore.c ndETimer1/ndETimer1.c ndFlash/ndFlash.c
+SOURCEFILES_QUOTED_IF_SPACED=ndFlash/ndFlash.c main.c ndEventBusCore/ndEventBusCore.c ndETimer1/ndETimer1.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/main.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1 ${OBJECTDIR}/ndETimer1/ndETimer1.p1 ${OBJECTDIR}/ndFlash/ndFlash.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/main.p1.d ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1.d ${OBJECTDIR}/ndETimer1/ndETimer1.p1.d ${OBJECTDIR}/ndFlash/ndFlash.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ndFlash/ndFlash.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1 ${OBJECTDIR}/ndETimer1/ndETimer1.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/ndFlash/ndFlash.p1.d ${OBJECTDIR}/main.p1.d ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1.d ${OBJECTDIR}/ndETimer1/ndETimer1.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/main.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1 ${OBJECTDIR}/ndETimer1/ndETimer1.p1 ${OBJECTDIR}/ndFlash/ndFlash.p1
+OBJECTFILES=${OBJECTDIR}/ndFlash/ndFlash.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1 ${OBJECTDIR}/ndETimer1/ndETimer1.p1
 
 # Source Files
-SOURCEFILES=main.c ndEventBusCore/ndEventBusCore.c ndETimer1/ndETimer1.c ndFlash/ndFlash.c
+SOURCEFILES=ndFlash/ndFlash.c main.c ndEventBusCore/ndEventBusCore.c ndETimer1/ndETimer1.c
 
 
 CFLAGS=
@@ -78,6 +78,14 @@ MP_PROCESSOR_OPTION=18F2523
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/ndFlash/ndFlash.p1: ndFlash/ndFlash.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/ndFlash 
+	@${RM} ${OBJECTDIR}/ndFlash/ndFlash.p1.d 
+	@${RM} ${OBJECTDIR}/ndFlash/ndFlash.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndFlash/ndFlash.p1  ndFlash/ndFlash.c 
+	@-${MV} ${OBJECTDIR}/ndFlash/ndFlash.d ${OBJECTDIR}/ndFlash/ndFlash.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/ndFlash/ndFlash.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/main.p1.d 
@@ -102,15 +110,15 @@ ${OBJECTDIR}/ndETimer1/ndETimer1.p1: ndETimer1/ndETimer1.c  nbproject/Makefile-$
 	@-${MV} ${OBJECTDIR}/ndETimer1/ndETimer1.d ${OBJECTDIR}/ndETimer1/ndETimer1.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/ndETimer1/ndETimer1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+else
 ${OBJECTDIR}/ndFlash/ndFlash.p1: ndFlash/ndFlash.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/ndFlash 
 	@${RM} ${OBJECTDIR}/ndFlash/ndFlash.p1.d 
 	@${RM} ${OBJECTDIR}/ndFlash/ndFlash.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndFlash/ndFlash.p1  ndFlash/ndFlash.c 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndFlash/ndFlash.p1  ndFlash/ndFlash.c 
 	@-${MV} ${OBJECTDIR}/ndFlash/ndFlash.d ${OBJECTDIR}/ndFlash/ndFlash.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/ndFlash/ndFlash.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
-else
 ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR} 
 	@${RM} ${OBJECTDIR}/main.p1.d 
@@ -134,14 +142,6 @@ ${OBJECTDIR}/ndETimer1/ndETimer1.p1: ndETimer1/ndETimer1.c  nbproject/Makefile-$
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndETimer1/ndETimer1.p1  ndETimer1/ndETimer1.c 
 	@-${MV} ${OBJECTDIR}/ndETimer1/ndETimer1.d ${OBJECTDIR}/ndETimer1/ndETimer1.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/ndETimer1/ndETimer1.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/ndFlash/ndFlash.p1: ndFlash/ndFlash.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/ndFlash 
-	@${RM} ${OBJECTDIR}/ndFlash/ndFlash.p1.d 
-	@${RM} ${OBJECTDIR}/ndFlash/ndFlash.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndFlash/ndFlash.p1  ndFlash/ndFlash.c 
-	@-${MV} ${OBJECTDIR}/ndFlash/ndFlash.d ${OBJECTDIR}/ndFlash/ndFlash.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/ndFlash/ndFlash.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
