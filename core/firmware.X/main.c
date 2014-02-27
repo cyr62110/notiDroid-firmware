5ad5d06db2973c507865992008c0403f3960fb9e
Cyril Vlaminck <developer@cvlaminck.fr>
2014-02-27 23:25:44 +0100

    WIP : Working on the event dispatcher
    Figured out how to make it work

--- core/firmware.X/main.c
+++ core/firmware.X/main.c
@@ -8,6 +8,8 @@
 #include "ndTimer2/ndTimer2.h"
 #include "ndTimer3/ndTimer3.h"
 
+void configureInterrupt();
+
 void interrupt isr() {
     if(dimmingTimerInterrupt()) {
         onDimmingTimerInterrupt();
@@ -34,29 +36,7 @@ int main(int argc, char** argv) {
     TRISA = 0;
     PORTAbits.RA0 = 0;
 
-    /* We configure the interruption */
-    INTCONbits.GIE = 1;
-    INTCONbits.PEIE = 1;
-
-    /* We will use high priority interrupt only for the dimming since it is the only real time operation  */
-    /* All other interrupt will be low priority */
-    RCONbits.IPEN = 1;
-    IPR2bits.TMR3IP = 1; //Dimming is high priority
-    IPR1bits.TMR1IP = 0;
-    
-
-    /* We configure the led driver */
-    initLedDriver();
-
-    /* We configure the PORTA to be handled by the module */
-    configureRegisterLedDriver(&PORTA, &TRISA, 0x01);
-
-    setDimmingHighLevelLenght(0, 16);
-    setBlinkingPeriod(0, 100);
-    setBlinkingHighLevelDuration(0, 50);
-
-    startTimer3();
-    startTimer1();
+    configureInterrupt();
 
     /* We will make some magic */
     //eraseFlash((uint24_t)0x5FE);
@@ -68,19 +48,30 @@ int main(int argc, char** argv) {
     //seekFlash((uint24_t)0x602);
     //flushFlash();
 
-    /* eventCode_t eventCode;
-    eventCode.eventStruct.isNotEmpty = 1;
-    eventCode.eventStruct.type = EVENT_TYPE_SW;
-    eventCode.eventStruct.moduleId = 2;
-    eventCode.eventStruct.eventId = 0x34;
-    dispatchEvent(eventCode); */
-
     //asm("goto 5FEh");
-    //on();
-    //test((void*)on); /* This is the trick behind all the eventbus architecture */
+    on();
+    test((void*)on); /* This is the trick behind all the eventbus architecture */
 
-    
+    PORTA = 0;
+
+    eventCode_t eventCode;
+    eventCode.eventStruct.isNotEmpty = 1;
+    eventCode.eventStruct.type = EVENT_TYPE_HW;
+    eventCode.eventStruct.moduleId = 1;
+    eventCode.eventStruct.eventId = 1;
+    dispatchEvent(eventCode);
 
     while(1);
 }
 
+void configureInterrupt() {
+    /* We configure the interruption */
+    INTCONbits.GIE = 1;
+    INTCONbits.PEIE = 1;
+
+    /* We will use high priority interrupt only for the dimming since it is the only real time operation  */
+    /* All other interrupt will be low priority */
+    RCONbits.IPEN = 1;
+    IPR2bits.TMR3IP = 1; //Dimming is high priority
+    IPR1bits.TMR1IP = 0;
+}
--- core/firmware.X/ndEventBusCore/ndEventBusCoreEventDispatcher.c
+++ core/firmware.X/ndEventBusCore/ndEventBusCoreEventDispatcher.c
@@ -3,17 +3,149 @@
 #include "ndEventBusCoreEventDispatcher.h"
 #include "ndEventBusCoreConstants.h"
 
+/**
+ * TODO
+ */
+void handleHardwareModuleEvent();
+
+/**
+ * 
+ */
+void onFirstHardwareModuleEvent();
+
+/**
+ * 
+ */
+void handleSoftwareModuleEvent();
+
+/**
+ * 
+ */
+void onFirstSoftwareModuleEvent();
+
 void dispatchEvent(eventCode_t eventCode) {
 	/* If it is an empty event, we have nothing to do */
-    if(eventCode.eventStruct.isNotEmpty)
+    if(!eventCode.eventStruct.isNotEmpty)
         return;
     /* Otherwise, we must load some register to send the eventcode to the event dispatcher ASM code. */
    PRODH = eventCode.eventStruct.moduleId;
    PRODL = eventCode.eventStruct.eventId;
     /* We call the modules block start address of the type of module we want */
     if(eventCode.eventStruct.type == EVENT_TYPE_HW) {
-        asm("goto 0x700");
+        handleHardwareModuleEvent();
     } else {
-        asm("goto 0x700");
+        handleSoftwareModuleEvent();
     }
 }
+
+void handleHardwareModuleEvent() {
+#asm
+    MOVF (PRODH), 0, 0 ; We copy PRODH in WREG
+    SUBLW 1 ; We substract to see if the module is the module 1
+    BNZ $+6
+    GOTO _onFirstHardwareModuleEvent
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+    NOP
+#endasm
+}
+
+void onFirstHardwareModuleEvent() {
+#asm
+    MOVF (PRODL), 0, 0
+    SUBLW 1
+    BNZ $+6 ; We skip the next instruction
+    GOTO 55Eh ;functionAddress ; We call the function registered to handle this event
+#endasm
+}
+
+void handleSoftwareModuleEvent() {
+    
+}
+
+void onFirstSoftwareModuleEvent() {
+    
+}
+
+
