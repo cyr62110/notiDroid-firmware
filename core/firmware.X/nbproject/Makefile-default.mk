5188470590ad3ec2bdd0457114f2200f7b2c8c26
Cyril Vlaminck <developer@cvlaminck.fr>
2014-02-27 20:18:12 +0100

    Useless commit

--- core/firmware.X/nbproject/Makefile-default.mk
+++ core/firmware.X/nbproject/Makefile-default.mk
@@ -45,17 +45,17 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Source Files Quoted if spaced
-SOURCEFILES_QUOTED_IF_SPACED=ndEeprom/ndEeprom.c ndEventBusCore/ndEventBusCore.c ndEventBusCore/ndEventBusCoreEventDispatcher.asm ndEventBusCore/ndEventBusCoreEventDispatcher.c ndFlash/ndFlash.c ndInternalMemory/ndInternalMemory.c ndLedDriver/ndLedDriver.c ndLedDriver/ndLedDriverEeprom.asm ndTimer0/ndTimer0.c ndETimer1/ndTimer1.c ndETimer1.c ndTimer2/ndTimer2.c ndTimer3/ndTimer3.c main.c ndLedDriver/ndLedDriverInitFunctions.c
+SOURCEFILES_QUOTED_IF_SPACED=ndEeprom/ndEeprom.c ndEventBusCore/ndEventBusCore.c ndEventBusCore/ndEventBusCoreEventDispatcher.asm ndEventBusCore/ndEventBusCoreEventDispatcher.c ndFlash/ndFlash.c ndInternalMemory/ndInternalMemory.c ndLedDriver/ndLedDriver.c ndLedDriver/ndLedDriverEeprom.asm ndLedDriver/ndLedDriverInitFunctions.c ndTimer0/ndTimer0.c ndETimer1/ndTimer1.c ndETimer1.c ndTimer2/ndTimer2.c ndTimer3/ndTimer3.c main.c
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ndEeprom/ndEeprom.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.obj ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.p1 ${OBJECTDIR}/ndFlash/ndFlash.p1 ${OBJECTDIR}/ndInternalMemory/ndInternalMemory.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriverEeprom.obj ${OBJECTDIR}/ndTimer0/ndTimer0.p1 ${OBJECTDIR}/ndETimer1/ndTimer1.p1 ${OBJECTDIR}/ndETimer1.p1 ${OBJECTDIR}/ndTimer2/ndTimer2.p1 ${OBJECTDIR}/ndTimer3/ndTimer3.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1
-POSSIBLE_DEPFILES=${OBJECTDIR}/ndEeprom/ndEeprom.p1.d ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1.d ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.obj.d ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.p1.d ${OBJECTDIR}/ndFlash/ndFlash.p1.d ${OBJECTDIR}/ndInternalMemory/ndInternalMemory.p1.d ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1.d ${OBJECTDIR}/ndLedDriver/ndLedDriverEeprom.obj.d ${OBJECTDIR}/ndTimer0/ndTimer0.p1.d ${OBJECTDIR}/ndETimer1/ndTimer1.p1.d ${OBJECTDIR}/ndETimer1.p1.d ${OBJECTDIR}/ndTimer2/ndTimer2.p1.d ${OBJECTDIR}/ndTimer3/ndTimer3.p1.d ${OBJECTDIR}/main.p1.d ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/ndEeprom/ndEeprom.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.obj ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.p1 ${OBJECTDIR}/ndFlash/ndFlash.p1 ${OBJECTDIR}/ndInternalMemory/ndInternalMemory.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriverEeprom.obj ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1 ${OBJECTDIR}/ndTimer0/ndTimer0.p1 ${OBJECTDIR}/ndETimer1/ndTimer1.p1 ${OBJECTDIR}/ndETimer1.p1 ${OBJECTDIR}/ndTimer2/ndTimer2.p1 ${OBJECTDIR}/ndTimer3/ndTimer3.p1 ${OBJECTDIR}/main.p1
+POSSIBLE_DEPFILES=${OBJECTDIR}/ndEeprom/ndEeprom.p1.d ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1.d ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.obj.d ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.p1.d ${OBJECTDIR}/ndFlash/ndFlash.p1.d ${OBJECTDIR}/ndInternalMemory/ndInternalMemory.p1.d ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1.d ${OBJECTDIR}/ndLedDriver/ndLedDriverEeprom.obj.d ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d ${OBJECTDIR}/ndTimer0/ndTimer0.p1.d ${OBJECTDIR}/ndETimer1/ndTimer1.p1.d ${OBJECTDIR}/ndETimer1.p1.d ${OBJECTDIR}/ndTimer2/ndTimer2.p1.d ${OBJECTDIR}/ndTimer3/ndTimer3.p1.d ${OBJECTDIR}/main.p1.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/ndEeprom/ndEeprom.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.obj ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.p1 ${OBJECTDIR}/ndFlash/ndFlash.p1 ${OBJECTDIR}/ndInternalMemory/ndInternalMemory.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriverEeprom.obj ${OBJECTDIR}/ndTimer0/ndTimer0.p1 ${OBJECTDIR}/ndETimer1/ndTimer1.p1 ${OBJECTDIR}/ndETimer1.p1 ${OBJECTDIR}/ndTimer2/ndTimer2.p1 ${OBJECTDIR}/ndTimer3/ndTimer3.p1 ${OBJECTDIR}/main.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1
+OBJECTFILES=${OBJECTDIR}/ndEeprom/ndEeprom.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCore.p1 ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.obj ${OBJECTDIR}/ndEventBusCore/ndEventBusCoreEventDispatcher.p1 ${OBJECTDIR}/ndFlash/ndFlash.p1 ${OBJECTDIR}/ndInternalMemory/ndInternalMemory.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1 ${OBJECTDIR}/ndLedDriver/ndLedDriverEeprom.obj ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1 ${OBJECTDIR}/ndTimer0/ndTimer0.p1 ${OBJECTDIR}/ndETimer1/ndTimer1.p1 ${OBJECTDIR}/ndETimer1.p1 ${OBJECTDIR}/ndTimer2/ndTimer2.p1 ${OBJECTDIR}/ndTimer3/ndTimer3.p1 ${OBJECTDIR}/main.p1
 
 # Source Files
-SOURCEFILES=ndEeprom/ndEeprom.c ndEventBusCore/ndEventBusCore.c ndEventBusCore/ndEventBusCoreEventDispatcher.asm ndEventBusCore/ndEventBusCoreEventDispatcher.c ndFlash/ndFlash.c ndInternalMemory/ndInternalMemory.c ndLedDriver/ndLedDriver.c ndLedDriver/ndLedDriverEeprom.asm ndTimer0/ndTimer0.c ndETimer1/ndTimer1.c ndETimer1.c ndTimer2/ndTimer2.c ndTimer3/ndTimer3.c main.c ndLedDriver/ndLedDriverInitFunctions.c
+SOURCEFILES=ndEeprom/ndEeprom.c ndEventBusCore/ndEventBusCore.c ndEventBusCore/ndEventBusCoreEventDispatcher.asm ndEventBusCore/ndEventBusCoreEventDispatcher.c ndFlash/ndFlash.c ndInternalMemory/ndInternalMemory.c ndLedDriver/ndLedDriver.c ndLedDriver/ndLedDriverEeprom.asm ndLedDriver/ndLedDriverInitFunctions.c ndTimer0/ndTimer0.c ndETimer1/ndTimer1.c ndETimer1.c ndTimer2/ndTimer2.c ndTimer3/ndTimer3.c main.c
 
 
 CFLAGS=
@@ -126,6 +126,14 @@ ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1: ndLedDriver/ndLedDriver.c  nbproject/Ma
 	@-${MV} ${OBJECTDIR}/ndLedDriver/ndLedDriver.d ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1: ndLedDriver/ndLedDriverInitFunctions.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/ndLedDriver 
+	@${RM} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d 
+	@${RM} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1  ndLedDriver/ndLedDriverInitFunctions.c 
+	@-${MV} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.d ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 ${OBJECTDIR}/ndTimer0/ndTimer0.p1: ndTimer0/ndTimer0.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/ndTimer0 
 	@${RM} ${OBJECTDIR}/ndTimer0/ndTimer0.p1.d 
@@ -173,14 +181,6 @@ ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/main.p1  main.c 
 	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1: ndLedDriver/ndLedDriverInitFunctions.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/ndLedDriver 
-	@${RM} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d 
-	@${RM} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  -D__DEBUG=1 --debugger=pickit3  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1  ndLedDriver/ndLedDriverInitFunctions.c 
-	@-${MV} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.d ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 else
 ${OBJECTDIR}/ndEeprom/ndEeprom.p1: ndEeprom/ndEeprom.c  nbproject/Makefile-${CND_CONF}.mk
@@ -231,6 +231,14 @@ ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1: ndLedDriver/ndLedDriver.c  nbproject/Ma
 	@-${MV} ${OBJECTDIR}/ndLedDriver/ndLedDriver.d ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/ndLedDriver/ndLedDriver.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
+${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1: ndLedDriver/ndLedDriverInitFunctions.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR}/ndLedDriver 
+	@${RM} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d 
+	@${RM} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1 
+	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1  ndLedDriver/ndLedDriverInitFunctions.c 
+	@-${MV} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.d ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d 
+	@${FIXDEPS} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
+	
 ${OBJECTDIR}/ndTimer0/ndTimer0.p1: ndTimer0/ndTimer0.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/ndTimer0 
 	@${RM} ${OBJECTDIR}/ndTimer0/ndTimer0.p1.d 
@@ -278,14 +286,6 @@ ${OBJECTDIR}/main.p1: main.c  nbproject/Makefile-${CND_CONF}.mk
 	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/main.p1  main.c 
 	@-${MV} ${OBJECTDIR}/main.d ${OBJECTDIR}/main.p1.d 
 	@${FIXDEPS} ${OBJECTDIR}/main.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
-	
-${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1: ndLedDriver/ndLedDriverInitFunctions.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/ndLedDriver 
-	@${RM} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d 
-	@${RM} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1 
-	${MP_CC} --pass1 $(MP_EXTRA_CC_PRE) --chip=$(MP_PROCESSOR_OPTION) -Q -G  --double=24 --float=24 --emi=wordwrite --opt=default,+asm,+asmfile,-speed,+space,-debug --addrqual=ignore --mode=free -P -N255 --warn=0 --asmlist --summary=default,-psect,-class,+mem,-hex,-file --output=default,-inhx032 --runtime=default,+clear,+init,-keep,-no_startup,-download,+config,+clib,+plib --output=-mcof,+elf:multilocs --stack=compiled:auto:auto:auto "--errformat=%f:%l: error: (%n) %s" "--warnformat=%f:%l: warning: (%n) %s" "--msgformat=%f:%l: advisory: (%n) %s"    -o${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1  ndLedDriver/ndLedDriverInitFunctions.c 
-	@-${MV} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.d ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d 
-	@${FIXDEPS} ${OBJECTDIR}/ndLedDriver/ndLedDriverInitFunctions.p1.d $(SILENT) -rsi ${MP_CC_DIR}../  
 	
 endif
 
